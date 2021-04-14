e7b5c62a54a19a2fa615f6898defcdfb75960acd
CXPhoenix <45819421+CXPhoenix@users.noreply.github.com>
2021-04-14 10:05:07 +0800

    rebuild: TextGameModule.py

---
+++ TextGameModuleV0.1.py
@@ -0,0 +1,70 @@
+"""
+這是 Text Game 的教學用模組
+"""
+import os
+import sys
+import time
+
+
+class TextGame:
+    def __init__(self, playerName: str='player'):
+        self.playerName = playerName
+        self.scene = {}
+        self.clearCmd = ''
+        self.playerRecord = {}
+
+        # detect system
+        if sys.platform.startswith('win32'):
+            self.clearCmd = 'cls'
+        elif sys.platform.startswith('darwin'):
+            self.clearCmd = 'clear'
+        else:
+            raise Exception('This game just support Windows and MacOS system..')
+    
+    """ about scene
+        Scene's format:
+        scene = {
+            sceneName:{
+                description -> str,
+                options -> str-list,
+                hiddenCondition -> list,
+                actions -> dict : {
+                    option -> str : go_to_scene -> str
+                }
+            },
+            ...
+        }
+    """
+    def setScene(self, sceneName: str, description: str, options: list, hiddenCondition: list=[]) -> None:
+        self.scene[sceneName] = {
+            'description': description,
+            'options': options,
+            'hiddenCondition': hiddenCondition,
+            'actions': {},
+        }
+    
+    def checkUploadSceneFormat(self, scene: dict, complete: bool = False) -> bool:
+        formatCondition = ['sceneName', 'description', 'options', 'hiddenCondition', 'actions']
+        if not complete:
+            formatCondition = formatCondition[:3]
+        for fc in formatCondition:
+            if not scene.get(fc):
+                return False
+                break
+        else:
+            return True
+        
+
+
+    def setSceneByDict(self, scene: dict) -> None:
+        
+        self.scene[scene.get(sceneName)]
+
+    def getFormatScene(self, sceneName: str, description: str, options: list, hiddenCondition: list=[], actions: dict={}) -> dict:
+        return {
+            'sceneName': sceneName,
+            'description': description,
+            'options': options,
+            'hiddenCondition': hiddenCondition,
+            'actions': actions
+        }
